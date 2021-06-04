44292e4b1a82d958804100f39fb53c22fa72703a
Daniel-n-huss <daniel.n.huss@gmail.com>
2021-06-04 12:50:43 -0600

    implement backstage specs

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -23,7 +23,54 @@ RSpec.describe GildedRose do
 
     context 'item is Backstage passes' do
       let(:backstage) { Item.new("Backstage passes to a TAFKAL80ETC concert", sell_in, quality) }
+      let(:sell_in) { 15 }
+      let(:quality) { 25 }
       let(:items) { [backstage] }
+
+      it 'decrements sell_in' do
+        expect { subject }.to change { backstage.sell_in }.by(-1)
+      end
+
+      context 'when sell_in is less than 10' do
+        let(:sell_in) { 9 }
+
+        it 'increases quality by 2' do
+          expect { subject }.to change { backstage.quality }.by(2)
+        end
+      end
+
+      context 'when sell_in is less than 5' do
+        let(:sell_in) { 3 }
+
+        it 'increases quality by 3' do
+          expect { subject }.to change { backstage.quality }.by(3)
+        end
+      end
+
+      context 'when sell_in is a negative number' do
+        let(:sell_in) { -1 }
+
+        it 'sets quality is 0' do
+          subject
+          expect(backstage.quality).to be 0
+        end
+      end
+
+      context 'when quality is below 50' do
+        let(:quality) { 25 }
+
+        it 'increases quality' do
+          expect { subject }.to change { backstage.quality }.by(1)
+        end
+      end
+
+      context 'when quality is at 50' do
+        let(:quality) { 50 }
+
+        it 'does not increase quality' do
+          expect { subject }.not_to change { backstage.quality }
+        end
+      end
     end
 
     context 'item is Brie' do
@@ -51,7 +98,6 @@ RSpec.describe GildedRose do
           expect { subject }.not_to change { brie.quality }
         end
       end
-
     end
 
     context 'item is Conjured Mana Cake' do
