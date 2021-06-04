0419eca9ad3360f501cf390c1ee6918f171772d0
Daniel-n-huss <daniel.n.huss@gmail.com>
2021-06-04 13:50:33 -0600

    simplify brie method

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -46,20 +46,12 @@ class GildedRose
   end
 
   def brie(item)
-    if item.quality < 50
-      item.quality = item.quality + 1
-    end
+    item.quality += 1 if item.quality < 50
 
     item.sell_in = item.sell_in - 1
 
     if item.sell_in < 0
-      if item.name != "Aged Brie"
-        #item.quality = item.quality - item.quality
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-        end
-      end
+      item.quality += 1 if item.quality < 50
     end
   end
 
