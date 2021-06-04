749abdf81b7aa7ba9514a21df47f8681338062de
Daniel-n-huss <daniel.n.huss@gmail.com>
2021-06-04 13:44:13 -0600

    extract backstage

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -10,29 +10,18 @@ class GildedRose
         sulfuras
       elsif item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
         default_item(item)
+      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+        backstage(item)
       else
-
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
         end
 
         item.sell_in = item.sell_in - 1
 
         if item.sell_in < 0
           if item.name != "Aged Brie"
-            item.quality = item.quality - item.quality
+            #item.quality = item.quality - item.quality
           else
             if item.quality < 50
               item.quality = item.quality + 1
@@ -51,6 +40,22 @@ class GildedRose
       item.quality -= 1
       item.sell_in -= 1
       item.quality -= 1 if item.sell_in < 0
+    end
+  end
+
+  def backstage(item)
+    item.sell_in -= 1
+
+    if item.sell_in < 0
+      item.quality = 0
+    else
+      item.quality += 1 if item.quality < 50
+      if item.sell_in < 11
+        item.quality = item.quality + 1 if item.quality < 50
+      end
+      if item.sell_in < 6
+        item.quality = item.quality + 1 if item.quality < 50
+      end
     end
   end
 
