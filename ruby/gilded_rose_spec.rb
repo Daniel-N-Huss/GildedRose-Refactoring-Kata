30e792803a0a135ffde681d7da86e21a9800481d
Daniel-n-huss <daniel.n.huss@gmail.com>
2021-06-04 15:04:53 -0600

    simplify spec

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -4,38 +4,36 @@ require "rspec"
 RSpec.describe GildedRose do
 
   describe "#update_quality" do
-    subject { described_class.new(items).update_quality }
+    subject { described_class.new([item]).update_quality }
 
     context 'item is Sulfuras' do
       let(:sell_in) { 10 }
       let(:quality) { 80 }
-      let(:sulfuras) { Item.new("Sulfuras, Hand of Ragnaros", sell_in, quality) }
-      let(:items) { [sulfuras] }
+      let(:item) { Item.new("Sulfuras, Hand of Ragnaros", sell_in, quality) }
 
       it 'does not change sell_in' do
-        expect { subject }.not_to change { sulfuras.sell_in }
+        expect { subject }.not_to change { item.sell_in }
       end
 
       it 'does not change quality' do
-        expect { subject }.not_to change { sulfuras.quality }
+        expect { subject }.not_to change { item.quality }
       end
     end
 
     context 'item is Backstage passes' do
-      let(:backstage) { Item.new("Backstage passes to a TAFKAL80ETC concert", sell_in, quality) }
+      let(:item) { Item.new("Backstage passes to a TAFKAL80ETC concert", sell_in, quality) }
       let(:sell_in) { 15 }
       let(:quality) { 25 }
-      let(:items) { [backstage] }
 
       it 'decrements sell_in' do
-        expect { subject }.to change { backstage.sell_in }.by(-1)
+        expect { subject }.to change { item.sell_in }.by(-1)
       end
 
       context 'when sell_in is 10 or less' do
         let(:sell_in) { 9 }
 
         it 'increases quality by 2' do
-          expect { subject }.to change { backstage.quality }.by(2)
+          expect { subject }.to change { item.quality }.by(2)
         end
       end
 
@@ -43,7 +41,7 @@ RSpec.describe GildedRose do
         let(:sell_in) { 3 }
 
         it 'increases quality by 3' do
-          expect { subject }.to change { backstage.quality }.by(3)
+          expect { subject }.to change { item.quality }.by(3)
         end
       end
 
@@ -52,7 +50,7 @@ RSpec.describe GildedRose do
 
         it 'sets quality is 0' do
           subject
-          expect(backstage.quality).to be 0
+          expect(item.quality).to be 0
         end
       end
 
@@ -60,7 +58,7 @@ RSpec.describe GildedRose do
         let(:quality) { 25 }
 
         it 'increases quality' do
-          expect { subject }.to change { backstage.quality }.by(1)
+          expect { subject }.to change { item.quality }.by(1)
         end
       end
 
@@ -68,26 +66,25 @@ RSpec.describe GildedRose do
         let(:quality) { 50 }
 
         it 'does not increase quality' do
-          expect { subject }.not_to change { backstage.quality }
+          expect { subject }.not_to change { item.quality }
         end
       end
     end
 
     context 'item is Brie' do
-      let(:brie) { Item.new("Aged Brie", sell_in, quality) }
+      let(:item) { Item.new("Aged Brie", sell_in, quality) }
       let(:sell_in) { 10 }
       let(:quality) { 25 }
-      let(:items) { [brie] }
 
       it 'decrements sell_in' do
-        expect { subject }.to change { brie.sell_in }.by(-1)
+        expect { subject }.to change { item.sell_in }.by(-1)
       end
 
       context 'when quality is below 50' do
         let(:quality) { 25 }
 
         it 'increases quality' do
-          expect { subject }.to change { brie.quality }.by(1)
+          expect { subject }.to change { item.quality }.by(1)
         end
       end
 
@@ -95,7 +92,7 @@ RSpec.describe GildedRose do
         let(:quality) { 50 }
 
         it 'does not increase quality' do
-          expect { subject }.not_to change { brie.quality }
+          expect { subject }.not_to change { item.quality }
         end
       end
     end
@@ -104,13 +101,12 @@ RSpec.describe GildedRose do
     end
 
     context 'any other items' do
-      let(:staff) { Item.new("Initiates Staff", sell_in, quality) }
+      let(:item) { Item.new("Initiates Staff", sell_in, quality) }
       let(:sell_in) { 10 }
       let(:quality) { 25 }
-      let(:items) { [staff] }
 
       it 'decrements sell_in' do
-        expect { subject }.to change { staff.sell_in }.by(-1)
+        expect { subject }.to change { item.sell_in }.by(-1)
       end
 
       context 'when quality is above 0' do
@@ -120,7 +116,7 @@ RSpec.describe GildedRose do
           let(:sell_in) { 10 }
 
           it 'decrements quality by 1' do
-            expect { subject }.to change { staff.quality }.by(-1)
+            expect { subject }.to change { item.quality }.by(-1)
           end
         end
 
@@ -128,7 +124,7 @@ RSpec.describe GildedRose do
           let(:sell_in) { -1 }
 
           it 'decrements quality by 2' do
-            expect { subject }.to change { staff.quality }.by(-2)
+            expect { subject }.to change { item.quality }.by(-2)
           end
         end
       end
@@ -137,7 +133,7 @@ RSpec.describe GildedRose do
         let(:quality) { 0 }
 
         it 'does not decrease quality' do
-          expect { subject }.not_to change { staff.quality }
+          expect { subject }.not_to change { item.quality }
         end
       end
     end
